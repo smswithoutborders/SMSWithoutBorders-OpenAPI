a3d7031137116a81a3a703931211f068fea2c96c
Promise Fru <promisefru1@gmail.com>
2022-04-05 13:21:10 +0100

    updated error exceptions for helper

--- routes/helpers.py
+++ routes/helpers.py
@@ -1,12 +1,26 @@
-
 import phonenumbers
 from phonenumbers import geocoder, carrier
 
-INVALID_PHONE_NUMBER_EXCEPTION = "INVALID PHONE NUMBER"
-INVALID_COUNTRY_CODE_EXCEPTION = "INVALID COUNTRY CODE"
-MISSING_COUNTRY_CODE_EXCEPTION = "MISSING COUNTRY CODE"
 
-def get_phonenumber_country(MSISDN:str)->str:
+class InvalidPhoneNUmber(Exception):
+    def __init__(self, message="INVALID PHONE NUMBER"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class InvalidCountryCode(Exception):
+    def __init__(self, message="INVALID COUNTRY CODE"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class MissingCountryCode(Exception):
+    def __init__(self, message="MISSING COUNTRY CODE"):
+        self.message = message
+        super().__init__(self.message)
+
+
+def get_phonenumber_country(MSISDN: str) -> str:
     """Returns the country of MSISDN.
     Args:
         MSISDN (str):
@@ -22,21 +36,19 @@ def get_phonenumber_country(MSISDN:str)->str:
     """
 
     try:
-        _number = phonenumbers.parse(MSISDN, 'en')
+        _number = phonenumbers.parse(MSISDN, "en")
 
         if not phonenumbers.is_valid_number(_number):
-            return Exception(INVALID_PHONE_NUMBER_EXCEPTION)
+            raise InvalidPhoneNUmber()
 
-        return \
-                phonenumbers.carrier.name_for_number(_number, 'en')
+        return phonenumbers.carrier.name_for_number(_number, "en")
 
     except phonenumbers.NumberParseException as error:
         if error.error_type == phonenumbers.NumberParseException.INVALID_COUNTRY_CODE:
-            if MSISDN[0] == '+' or MSISDN[0] == '0':
-                raise Exception(INVALID_COUNTRY_CODE_EXCEPTION)
+            if MSISDN[0] == "+" or MSISDN[0] == "0":
+                raise InvalidCountryCode()
             else:
-                raise Exception(MISSING_COUNTRY_CODE_EXCEPTION)
-
+                raise MissingCountryCode()
         else:
             raise error
 
